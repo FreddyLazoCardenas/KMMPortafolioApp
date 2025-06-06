b47243ceb2e60cef678e8a53f19aed21057657f1
Freddy Lazo <freddy.lazo@rappi.com>
2025-06-06 12:10:44 -0500

    Revert "fix web height"
    This reverts commit fb63255d8d35e878a8697c855f09b9bcd2cedc41.

--- composeApp/src/wasmJsMain/kotlin/org/freddy/project/main.kt
+++ composeApp/src/wasmJsMain/kotlin/org/freddy/project/main.kt
@@ -3,13 +3,10 @@ package org.freddy.project
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
-import org.w3c.dom.HTMLElement
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    val rootElement = document.getElementById("root") as HTMLElement
-    ComposeViewport(rootElement) {
+    ComposeViewport(document.body!!) {
         App()
     }
 }
-
