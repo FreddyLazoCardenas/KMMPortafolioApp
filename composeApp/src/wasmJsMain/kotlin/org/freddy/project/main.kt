fb63255d8d35e878a8697c855f09b9bcd2cedc41
Freddy Lazo <freddy.lazo@rappi.com>
2025-06-06 11:56:16 -0500

    fix web height

--- composeApp/src/wasmJsMain/kotlin/org/freddy/project/main.kt
+++ composeApp/src/wasmJsMain/kotlin/org/freddy/project/main.kt
@@ -3,10 +3,13 @@ package org.freddy.project
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import org.w3c.dom.HTMLElement
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    ComposeViewport(document.body!!) {
+    val rootElement = document.getElementById("root") as HTMLElement
+    ComposeViewport(rootElement) {
         App()
     }
 }
+
