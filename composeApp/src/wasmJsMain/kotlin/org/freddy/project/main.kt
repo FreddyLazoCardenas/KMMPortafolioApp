bcb61872fb9b0f62f12bada18a2c3d4d9460925d
Freddy Lazo <freddy.lazo@rappi.com>
2025-06-06 23:36:22 -0500

    trying solve web page error

--- composeApp/src/wasmJsMain/kotlin/org/freddy/project/main.kt
+++ composeApp/src/wasmJsMain/kotlin/org/freddy/project/main.kt
@@ -3,10 +3,34 @@ package org.freddy.project
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import kotlinx.browser.window
+import org.w3c.dom.HTMLElement
+import org.w3c.dom.css.CSSStyleDeclaration
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    ComposeViewport(document.body!!) {
+    // Get the body element
+    val body = document.body!!
+
+    // Configure the body and HTML for full height
+    configureFullHeight(document.documentElement as HTMLElement)
+    configureFullHeight(body)
+
+    // Set up the Compose viewport
+    ComposeViewport(body) {
         App()
     }
 }
+
+/**
+ * Configure an HTML element to take full height
+ */
+private fun configureFullHeight(element: HTMLElement) {
+    val style: CSSStyleDeclaration = element.style
+    style.setProperty("width", "100%")
+    style.setProperty("height", "100vh")
+    style.setProperty("margin", "0")
+    style.setProperty("padding", "0")
+    style.setProperty("overflow", "hidden")
+}
+
